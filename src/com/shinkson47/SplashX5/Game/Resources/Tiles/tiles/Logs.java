aa764ee54429b34bf3f35fa2e8a6adfa720d188c
Shinkson47 <50697488+Shinkson47@users.noreply.github.com>
2019-05-23 21:44:35 +0100

    Crafting bench
    Textures
    Added interaction events
    Crafting bench window

--- src/com/shinkson47/SplashX5/Game/Resources/Tiles/tiles/Logs.java
+++ src/com/shinkson47/SplashX5/Game/Resources/Tiles/tiles/Logs.java
@@ -6,7 +6,7 @@ import com.shinkson47.SplashX5.Game.Resources.ResourceManager;
 import com.shinkson47.SplashX5.Interfaces.ITile;
 
 public class Logs implements ITile {
-	public static Image Texture = ResourceManager.getTexture("Tiles/CraftingTable");
+	public static Image Texture = ResourceManager.getTexture("Tiles/Logs");
 	public static boolean Walkable = false, CausesDamage = false, IsFluid = false, IsHarvestable = true, CausesEvent = true, SupportsForeTile = false, SupportsBaseTile = false;
 	public static int DamageMultiplyer = 1, SpeedReduction = 0, Hardness = 1;
 	public static String TileData = "", FriendlyName = "Wooden Logs", TileDrop = "Logs";
